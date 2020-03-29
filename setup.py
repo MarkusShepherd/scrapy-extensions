da377ce08d021cc610870736ba6e9f8f81327c8d
Markus Shepherd <markus.schepke@maas.global>
2020-03-29 08:48:29 +0300

    added readability

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ VERSION = None  # will be read from __version__.py
 REQUIRED = ("pytility", "jmespath", "Scrapy", "Twisted", "w3lib")
 
 # What packages are optional?
-EXTRAS = {}
+EXTRAS = {"articles": ("readability-lxml",)}
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
