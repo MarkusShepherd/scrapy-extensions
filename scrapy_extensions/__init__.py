73927d2346e294704513e41ff06a161b8ce246b1
Markus Shepherd <markus.schepke@maas.global>
2020-04-14 15:15:08 +0300

    updated top level imports

--- scrapy_extensions/__init__.py
+++ scrapy_extensions/__init__.py
@@ -10,8 +10,9 @@ from .extensions import (
     MultiFeedExporter,
     NicerAutoThrottle,
 )
-from .items import TypedItem
-from .loaders import JsonLoader
+from .items import URL_PROCESSOR, DATE_PROCESSOR, ArticleItem, TypedItem, WebpageItem
+from .loaders import ArticleLoader, JsonLoader, WebpageLoader
 from .loggers import QuietLogFormatter
 from .middleware import DelayedRetry
 from .pipelines import ValidatePipeline
+from .spiders import ArticleSpider, WebsiteSpider
