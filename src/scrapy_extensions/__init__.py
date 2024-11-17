134f49acfacd7c025a9cae7c284d04b1facd578b
Markus Schepke <markus.schepke@wolt.com>
2024-11-17 22:58:44 +0200

    BlurHashPipeline

--- src/scrapy_extensions/__init__.py
+++ src/scrapy_extensions/__init__.py
@@ -1,6 +1,3 @@
-# TODO:
-# - BlurHashPipeline
-
 # Implemented by now:
 # - https://doc.scrapy.org/en/latest/topics/loaders.html#scrapy.loader.ItemLoader.add_jmes
 # - https://docs.scrapy.org/en/latest/topics/extensions.html?highlight=periodiclog#periodic-log-extension
@@ -9,8 +6,10 @@
 from scrapy_extensions.downloadermiddlewares import DelayedRetryMiddleware
 from scrapy_extensions.extensions import LoopingExtension, NicerAutoThrottle
 from scrapy_extensions.loggers import QuietLogFormatter
+from scrapy_extensions.pipelines import BlurHashPipeline
 
 __all__ = [
+    "BlurHashPipeline",
     "DelayedRetryMiddleware",
     "LoopingExtension",
     "NicerAutoThrottle",
