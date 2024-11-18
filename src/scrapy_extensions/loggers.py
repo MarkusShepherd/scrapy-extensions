2f45c103dbb08ab007736187300867202d1bd9c5
Markus Schepke <markus.schepke@wolt.com>
2024-11-18 23:30:59 +0200

    Better imports in src/scrapy_extensions/loggers.py

--- src/scrapy_extensions/loggers.py
+++ src/scrapy_extensions/loggers.py
@@ -2,21 +2,23 @@
 
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
-import scrapy
-import scrapy.http
-import scrapy.logformatter
+from scrapy.logformatter import LogFormatter
+
+if TYPE_CHECKING:
+    from scrapy import Spider
+    from scrapy.http import Response
 
 
-class QuietLogFormatter(scrapy.logformatter.LogFormatter):
+class QuietLogFormatter(LogFormatter):
     """Be quieter about scraped items."""
 
     def scraped(
         self,
         item: Any,
-        response: scrapy.http.Response,
-        spider: scrapy.Spider,
+        response: Response,
+        spider: Spider,
     ) -> dict[str, Any] | None:
         return (
             super().scraped(item, response, spider)
